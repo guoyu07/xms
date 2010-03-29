ae04a4cb2228f97cdd8854df552479e54ad8bcb8
Khashayar Hajian <me@khashayar.me>
2010-03-29 01:00:36 +0000

    Template

---
+++ app/lib/view/XRXFileTemplateLayer.class.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Template layer implementation for templates fetched using a PHP stream.
+ *
+ * @author     Khashayar Hajian <me@khashayar.me>
+ * @copyright  Authors
+ * @copyright  The XRX Project
+ */
+class XRXFileTemplateLayer extends AgaviFileTemplateLayer
+{
+	/**
+	 * Constructor.
+	 *
+	 * @param      array Initial parameters.
+	 *
+	 * @author     Khashayar Hajian <me@khashayar.me>
+	 */
+	public function __construct(array $parameters = array())
+	{
+		$targets = array();
+		if(AgaviConfig::get('core.use_translation')) {
+			$targets[] = AgaviConfig::get('core.template_dir') . '/ctpl/${module}/${locale}/${template}${extension}';
+			$targets[] = AgaviConfig::get('core.template_dir') . '/ctpl/${module}/${template}.${locale}${extension}';
+			$targets[] = '${directory}/${locale}/${template}${extension}';
+			$targets[] = '${directory}/${template}.${locale}${extension}';
+		}
+		$targets[] = AgaviConfig::get('core.template_dir') . '/ctpl/${module}/${template}${extension}';
+		$targets[] = '${directory}/${template}${extension}';
+		
+		parent::__construct(array_merge(array(
+			'directory' => AgaviConfig::get('core.module_dir') . '/${module}/templates',
+			'scheme' => 'file',
+			'check' => true,
+			'targets' => $targets,
+		), $parameters));
+	}
+}
+
+?>
